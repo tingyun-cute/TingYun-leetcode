979f55b05656918059f1124da642c680f7e5211c
lf.deng <1919794196@qq.com>
2025-04-08 22:57:54 +0800

    test

--- leetcode1/src/binary/FindMin.java
+++ leetcode1/src/binary/FindMin.java
@@ -2,6 +2,21 @@ package binary;
 
 public class FindMin {
     public int findMin(int[] nums) {
-        return 1;
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                // 最小值在右半部分
+                left = mid + 1;
+            } else {
+                // 最小值在左半部分或当前mid
+                right = mid;
+            }
+        }
+
+        return nums[left];
     }
 }
